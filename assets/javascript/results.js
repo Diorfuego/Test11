b405f93c6a1df29a4f5b9017bee7826328f40e1e
Dior Fuego <diorfuego@hotmail.com>
2019-09-16 02:03:14 -0700

    update to results

---
+++ assets/javascript/results.js
@@ -0,0 +1,19 @@
+$( "#w-and-l" ).click(function() {
+  alert( "weather & location .click() called." );
+});
+
+$( "#t-and-d" ).click(function() {
+    alert( "time & date .click() called." );
+  });
+
+  $( "#event1" ).click(function() {
+    alert( "event#1 .click() called." );
+  });
+
+  $( "#event2" ).click(function() {
+    alert( "event2 .click() called." );
+  });
+
+  $( "#reset-all" ).click(function() {
+    alert( "reset-all .click() called." );
+  });
